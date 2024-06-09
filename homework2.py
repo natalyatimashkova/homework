d9f93a3e4052c38a60f705476d04560bf35544fe
natalyatimashkova <natalyatimashkova@mail.ru>
2024-06-09 13:35:41 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_tasks = "12"
+number_hours = "1.5"
+name_cours = "Python"
+time_task = (1.5 / 12)
+print("Курс: " + name_cours + ', ' "всего задач: " + number_tasks + ',' ' затрачено часов: ' +
+      number_hours + ',' ' среднее время выполнения: ' + str(time_task))
