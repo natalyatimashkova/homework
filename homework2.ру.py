9df077fd06f699a4ed02abea54c3c6c230b523ce
natalyatimashkova <natalyatimashkova@mail.ru>
2024-06-11 20:44:59 +0300

    Delete homework2.ру.py

--- homework2.ру.py
+++
@@ -1,6 +0,0 @@
-a = number_tasks = "12"
-b = number_hours = "1.5"
-c = name_cours = "Python"
-time_task = (1.5 / 12)
-print("Курс: " + name_cours + ', ' "всего задач: " + number_tasks + ',' ' затрачено часов: ' +
-      number_hours + ',' ' среднее время выполнения: ' + str(time_task))
