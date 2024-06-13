8885f07a46130ddb610366a9cc8ae5b0e6a1aca7
natalyatimashkova <natalyatimashkova@mail.ru>
2024-06-13 09:50:18 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('Куда вы поедете в отпуск? ') #Я поеду в Китай
+print(len(my_string))    #15
+print(my_string.upper()) #Я ПОЕДУ В КИТАЙ
+print(my_string.lower()) #я поеду в китай
+print(my_string.replace(' ', ''))  #ЯпоедувКитай
+print(my_string[0])  #Я
+print(my_string[-1])  #й
