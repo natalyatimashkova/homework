09a0f049516d37ae54f3d9a280f5ce5f5fd5833c
natalyatimashkova <natalyatimashkova@mail.ru>
2024-06-13 11:30:58 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -2,7 +2,7 @@ immutable_var = (1, 2, 3, True, 'String')
 print(immutable_var)
 immutable_var[1] = 5 #кортеж является неизменяемым (immutable) типом
 print(immutable_var)
-mutable_list = ['orange', 'pear', 'banana', 'kiwi']
-print(mutable_list)
-mutable_list[1:2] = 'cat', 'dog'
-print(mutable_list)
+imutable_list = ['orange', 'pear', 'banana', 'kiwi']
+print(imutable_list)
+imutable_list[1:2] = 'cat', 'dog'
+print(imutable_list)
