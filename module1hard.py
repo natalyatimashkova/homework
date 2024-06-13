701023453d53b28aa60bebe30d81d4ef3fd3bfe8
natalyatimashkova <natalyatimashkova@mail.ru>
2024-06-13 22:28:01 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,13 @@
+Aaron = [5,3,3,5,4]
+average_Aaron = sum(Aaron)/len(Aaron)
+Bildo = [2,2,2,3]
+average_Bildo = sum(Bildo)/len(Bildo)
+Johnny = [4, 5, 5, 2]
+average_Johnny = sum(Johnny)/len(Johnny)
+Khendrik = [4, 4, 3]
+average_Khendrik = sum(Khendrik)/len(Khendrik)
+Steve = [5, 5, 5, 4, 5]
+average_Steve = sum(Steve)/len(Steve)
+average_score = {'Aaron': average_Aaron, 'Bildo': average_Bildo, 'Johnny': average_Johnny,
+                 'Khendrik': average_Khendrik, 'Steve': average_Steve}
+print(average_score)
