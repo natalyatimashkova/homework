950b2f2b4306dffed12f1e44f129a51b571860bc
natalyatimashkova <natalyatimashkova@mail.ru>
2024-07-04 13:26:03 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as f_m
+import true_math as t_m
+
+result1 = f_m(69, 3)
+result2 = f_m(3, 0)
+result3 = t_m(49, 7)
+result4 = t_m(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
